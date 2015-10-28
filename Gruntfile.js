a305e3a1655ae796d5465e19768727aca710fc56
David Nichols <dnich@davids-mbp.opentext.net>
2015-10-28 11:04:38 -0400

    Compile jade templates, get a local server going

---
+++ Gruntfile.js
@@ -0,0 +1,170 @@
+var config = function (grunt) {
+  
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    // Compile Jade templates
+    // Ex: grunt jade --[dev, production]
+    
+    jade: {
+      
+      //Target: dev
+      dev : {
+        options: {
+          pretty: true,
+          data: {
+            debug: true
+          }
+        },
+        files: {
+          'src/index.html': 'src/templates/index-dev.jade'
+        }
+      },
+
+      // Target: production
+      production: {
+        options: {
+          pretty: true,
+          data: {
+            debug: false
+          }
+        },
+        files: {
+          'src/index.html': 'src/templates/index-production.jade'
+        }
+      }
+      
+    },
+
+    // Minify CSS
+    //Ex: grunt cssmin
+    
+    cssmin: {
+
+      // Target: production
+      production: {
+        src: 'app/assets/style/compiled/app.css',
+        dest: 'app/dist/app.min.css',
+      }
+    },
+
+    // Minify JS
+    // Ex: grunt uglify
+    
+    uglify: {
+
+      // Options
+      options: {
+        report: 'min',
+        mangle: true
+      },
+
+      // Target: production
+      production: {
+        files: [
+          {
+            src: [
+              // main module
+              'app/app.js',
+              // any additional lib files not managed by bower or npm
+              'app/assets/js/lib/*.js',
+              // global controllers/directives/services
+              'app/global/*.js',
+              'app/global/**/*.js',
+              // app modules                           
+              'app/modules/*.js',           
+              'app/modules/**/*.js',
+            ],
+            dest: 'app/dist/app.min.js'
+          }
+        ]
+      }
+    },
+    
+    // Watch for file changes
+    // Ex: grunt watch
+    watch: {
+      script: {
+        files: [
+          'app/templates/*.jade', 
+          'app/templates/partials/*.jade', 
+          'app/assets/style/*.scss'
+        ],
+        tasks: ['build-dev'],
+        options: {
+          spawn: false,
+          reload: true
+        }
+      }  
+    },
+    
+    // Static analysis
+    jshint: {
+      files: {
+        src: [
+          'app.js',
+          'app/config/*.js',
+          'app/modules/*.js',
+          'app/modules/**/*.js',
+          'app/global/*.js',
+          'app/global/**/*.js'
+        ]
+      }
+    },
+    
+    // Compile sass
+    sass: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'src/assets/style',
+          src: ['*.scss'],
+          dest: 'src/assets/style/compiled',
+          ext: '.css'
+        }]
+      }
+    },
+    
+    // Copy files
+    copy: {
+      production: {
+        files: [
+          // includes files within path
+          { 
+            expand: true, 
+            src: ['node_modules/bricks-css/dist/bricks.min.css', 'node_modules/doorman-js/dist/doorman.min.js'], 
+            dest: 'app/dist/assets/', 
+            flatten: true, 
+            filter: 'isFile'
+          },
+        ],
+      },
+    },
+    
+    // Test runner
+    karma: {
+      unit: {
+        configFile: 'karma.conf.js'
+      }
+    }
+    
+  });
+  
+  // Load tasks
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-karma');
+  
+  // Register custom tasks
+  grunt.registerTask('build-dev', ['sass', 'jade:dev']);
+  grunt.registerTask('build-prod', ['sass', 'cssmin:production', 'jade:production', 'uglify:production', 'copy:production']);
+    
+};
+
+module.exports = config;
---
+++ src/modules/dependencies.js
@@ -0,0 +1,12 @@
+// Register all module dependencies here
+
+(function (angular) {
+
+  'use strict';
+
+  // Nodes
+  angular.module('bebop.nodes', [
+    'ngRoute'
+  ]);
+
+})(angular);
