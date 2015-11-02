5c49f8181940d702c03c4d6ffab80b62eae6bdc0
nicholsd <davenich@gmail.com>
2015-11-02 11:58:58 -0500

    Adding some routes

--- src/modules/nodes/routes.js
+++ src/modules/nodes/routes.js
@@ -6,7 +6,20 @@
   
   .config(['$routeProvider', function ($routeProvider) {
     
-    $routeProvider.when('/n/:id', {
+    // A list of top-level nodes
+    $routeProvider.when('/n', {
+      controller: 'nodeController',
+      templateUrl: 'modules/nodes/views/nodes.html'
+    });
+    
+    // A specific top-level node
+    $routeProvider.when('/n/:pid', {
+      controller: 'nodeController',
+      templateUrl: 'modules/nodes/views/nodes.html'
+    });
+    
+    // A child node
+    $routeProvider.when('/n/:pid/:cid', {
       controller: 'nodeController',
       templateUrl: 'modules/nodes/views/nodes.html'
     });
