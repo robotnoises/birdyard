141a4255bd35a5cda540c422978a9894caf88eb4
nicholsd <davenich@gmail.com>
2015-11-10 14:12:57 -0500

    Add an escape directive

---
+++ src/modules/utilities/directives/escape.js
@@ -0,0 +1,27 @@
+(function (angular) {
+  
+  'use strict';
+  
+  angular.module('bebop.utilities')
+  
+  .directive('escape', function () {
+    
+    return function (scope, element, attrs) {
+    
+      // Bind keydown events
+      element.bind('keydown', function (e) {
+        
+        var key = e.keyCode || e.charCode;
+        
+        // If the key is escape     
+        if (key == 27) {
+          scope.$apply(function () {
+            // evaluate whatever function was passed-in
+            scope.$eval(attrs['escape']);
+          });
+        }
+      });
+    }
+  });
+  
+})(angular);
