5fe4de3b399abb439dab64541dcbb17e7bed19c2
nicholsd <davenich@gmail.com>
2015-11-20 16:19:59 -0500

    Housekeeping

--- src/modules/notifications/services/notification-svc.js
+++ src/modules/notifications/services/notification-svc.js
@@ -52,16 +52,18 @@
       
       return $q(function (resolve, reject) {
         // Todo check if this is the same user
+
         // Get a handle on the user to be notified
         var $items = firebaseService.getRef('notifications', userId, 'items');
         var now = $window.Firebase.ServerValue.TIMESTAMP;
+        
         // Note: this will always overwrite existing notifications (we want that)
         $items.child(id).setWithPriority({
           'where': where,
           'type': notificationType,
           'id': id,
           'timestamp': now
-        }, (0 - Date.now()));
+        }, (0 - Date.now())); // sort descending
         
         return $items.on('child_added', function ($snap) {
           resolve();
