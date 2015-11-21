fbd9190162929fb5e756f6d3785a60d34b1a8120
David Nichols <davenich@gmail.com>
2015-11-20 22:00:04 -0500

    Adding style to notifications panel

---
+++ src/modules/utilities/directives/myTime.js
@@ -0,0 +1,20 @@
+(function (angular, moment) {
+  
+  'use strict';
+  
+  angular.module('bebop.utilities')
+  
+  .directive('myTime', function () {
+    return {
+      restrict: 'A',
+      replace: false,
+      template: '<span class="strong">{{date}}</span>',
+      link: function (scope, element, attrs) {
+        var utc = parseInt(attrs['myTime'], 10);
+        var date = moment(utc).local();
+        scope.date = date.format('MMM D') + ' at ' + date.format('h:mm a');
+      }
+    }
+  });
+  
+})(angular, moment);
