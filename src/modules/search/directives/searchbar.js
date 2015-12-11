58fc00c4889f79718b480a4d2d49853c498e6a68
nicholsd <davenich@gmail.com>
2015-12-11 16:05:22 -0500

    New: Implementing basic search (no ui)

--- src/modules/search/directives/searchbar.js
+++ src/modules/search/directives/searchbar.js
@@ -4,7 +4,7 @@
   
   angular.module('bebop.search')
   
-  .directive('searchBar', function () {
+  .directive('searchBar', ['searchService', function (searchService) {
     
     return {
       restrict: 'E',
@@ -12,8 +12,19 @@
       templateUrl: 'modules/search/views/searchbar.html',
       link: function (scope, element, attrs) {
         
+        // Bind keydown events
+        element.bind('keydown', function (e) {
+          
+          var key = e.keyCode || e.charCode;
+          
+          // If the key is enter     
+          if (key == 13) {
+            searchService.search(scope.search, 'room');
+          }
+        });
+        
       }
     }
-  });
+  }]);
   
 })(angular);
--- src/modules/search/services/search-svc.js
+++ src/modules/search/services/search-svc.js
@@ -2,13 +2,32 @@
   
   'use strict';
   
-  angular.module('bebop.search')
+  var ELASTIC_INDEX = 'bebop';
   
-  // Pun
+  
+  angular.module('bebop.search')
+    
   .factory('searchService', ['$q', 'firebaseService', 
   
   function ($q, firebaseService) {
+    
+    var _searchService = {};
+    
+    function _search(query, type) {
 
+      var $ref = firebaseService.getRef('search');
+      var $key = $ref.child('request').push({ index: ELASTIC_INDEX, type: type, query: query }).key();
+      
+      $ref.child('response/'+ $key).on('value', function ($snap) {
+        console.log($snap.val());
+      });
+   
+    }
+    
+    _searchService.search = _search;
+    
+    return _searchService;
+    
   }]);
   
 })(angular);
