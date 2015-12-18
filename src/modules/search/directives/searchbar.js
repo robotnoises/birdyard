184fce43c9e5ed56da058afc9777f498e3698f19
nicholsd <davenich@gmail.com>
2015-12-18 09:33:36 -0500

    Progress indicator for search

--- src/modules/search/directives/searchbar.js
+++ src/modules/search/directives/searchbar.js
@@ -14,34 +14,45 @@
       templateUrl: 'modules/search/views/searchbar.html',
       link: function (scope, element, attrs) {
         
+        var $t;
+        
+        function doSearch() {
+          searchService.search(scope.search, 'room').then(function($results) {
+            scope.searching = false;
+            if ($results) {
+              scope.results = $results.hits; 
+            }
+          }).catch(function (error) {
+            console.error(error);
+            scope.searching = false;
+          });
+        }
+        
+        scope.$watch('search', function (value, lastValue) {
+          
+          // If there's a previous timeout, cancel it
+          if ($t) $timeout.cancel($t);
+          
+          // Don't preceed unless there is a search, it's new, and it's at least 3 chars
+          if (!value || value === lastValue || value.length <= 2) return;
+          
+          scope.searching = true;
+          
+          // Perform the search
+          $t = $timeout(doSearch, 500);
+          
+        });
+        
+        // Public
+        
         scope.results = [];
+        scope.searching = false;
         
         scope.goTo = function (nodeId) {
           $timeout(function () {
             $location.path('/n/' + nodeId);  
           }, 300);
         };
-
-        // Bind keydown events
-        element.bind('keydown', function (e) {
-          
-          var key = e.keyCode || e.charCode;
-          
-          // If the key is enter     
-          if (key == 13) {
-            searchService.search(scope.search, 'room').then(function($results) {
-              console.log($results);
-              if ($results) {
-                //$timeout(function () {
-                  scope.results = $results.hits; 
-                //});
-              }
-            }).catch(function (error) {
-              console.error(error);
-            });
-          }
-        });
-        
       }
     }
   }]);
