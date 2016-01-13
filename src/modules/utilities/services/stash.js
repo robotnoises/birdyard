1a0222d501aabfe248934f236444735f681bca29
nicholsd <davenich@gmail.com>
2016-01-13 16:27:47 -0500

    Refactor stash service

--- src/modules/utilities/services/stash.js
+++ src/modules/utilities/services/stash.js
@@ -29,24 +29,9 @@
       }
     }
     
-    // Public
-    
-    var _stashService = {};
-    
-    function _set(key, value) {
-      if (key && value) {
-        if (isObject(value)) {
-          value = JSON.stringify(value);
-        }
-        $window.sessionStorage.setItem(key, value);
-      } else {
-        throw new Error('A Key and Value must be provided.');
-      }
-    }
-    
-    function _get(key) {
+    function get(key, storageType) {
       if (key) {
-        var value = $window.sessionStorage.getItem(key);
+        var value = $window[storageType].getItem(key);
         if (isSerializedObject(value)) {
           return JSON.parse(value);
         } else {
@@ -56,6 +41,21 @@
         throw new Error('A Key must be provided.');
       }
     }
+
+    function set(key, value, storageType) {
+      if (key && value) {
+        if (isObject(value)) {
+          value = JSON.stringify(value);
+        }
+        $window[storageType].setItem(key, value);
+      } else {
+        throw new Error('A Key and Value must be provided.');
+      }
+    }
+    
+    // Public
+    
+    var _stashService = {};
     
     function _exists(key) {
       if (key) {
@@ -65,15 +65,20 @@
       }
     }
     
+    function _get(key) {
+      get(key, 'sessionStorage');
+    }
+    
+    function _getLongTerm(key) {
+      get(key, 'localStorage');
+    }
+    
+    function _set(key, value) {
+      set(key, value, 'sessionStorage');
+    }
+    
     function _setLongTerm(key, value) {
-      if (key && value) {
-        if (isObject(value)) {
-          value = JSON.stringify(value);
-        }
-        $window.localStorage.setItem(key, value);
-      } else {
-        throw new Error('A Key and Value must be provided.');
-      }
+      set(key, value, 'localStorage');
     }
     
     _stashService.set = _set;
