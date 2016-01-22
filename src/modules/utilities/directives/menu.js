339037e230ae5e7838c168d754257171cd5623db
nicholsd <davenich@gmail.com>
2016-01-22 13:52:36 -0500

    Menu icon tweaks

--- src/modules/utilities/directives/menu.js
+++ src/modules/utilities/directives/menu.js
@@ -31,9 +31,9 @@
         
         '<div class="birdyard-btn pointer" ng-hide="signedIn" ng-click="signIn()">Sign in with Twitter</div>' + 
         
-        '<div ng-if="signedIn" class="birdyard-btn pointer" style="padding-bottom: 0; height: 33px; width: 33px; text-align: center;" ng-click="toggleMenu()">' + 
-          '<i class="icon fa fa-navicon menu" ng-if="notificationCount == 0" style="font-size: 18px;"></i>' + 
-          '<i class="icon fa fa-bell co-yellow menu ding" ng-if="notificationCount > 0" ng-class="{\'ring\': ring}" style="font-size: 18px; text-shadow: 0px 1px 0px rgba(0,0,0,0.5)"></i>' +
+        '<div ng-if="signedIn" class="birdyard-btn pointer" style="padding-bottom: 0; height: 33px; width: 33px; text-align: center; overflow: hidden;" ng-click="toggleMenu()">' + 
+          '<i class="icon fa fa-navicon menu" ng-if="notificationCount == 0" style="font-size: 20px;"></i>' + 
+          '<i class="icon fa fa-bell co-yellow menu ding" ng-if="notificationCount > 0" ng-class="{\'ring\': ring}" style="font-size: 20px; text-shadow: 0px 1px 0px rgba(0,0,0,0.5);"></i>' +
         '</div>' +
 
         '<div class="birdyard-menu" ng-class="{show: showing}" ng-click="close()">' +
