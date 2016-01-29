f6e67c942af7fae829b1075f9c3bc021352954ec
David Nichols <davenich@gmail.com>
2016-01-29 16:40:19 -0500

    Configure some themes, add more form field validation

--- src/config/configs/themes.js
+++ src/config/configs/themes.js
@@ -6,10 +6,33 @@
   
   .config(['$mdThemingProvider', function ($mdThemingProvider) {
     
+    // Define Palettes
+    
+    // $mdThemingProvider.definePalette('by-grey', {
+    //   '50': 'fafafa',
+    //   '100': 'f7f7f7',
+    //   '200': 'eeeeee',
+    //   '300': 'dddddd',
+    //   '400': 'cccccc',
+    //   '500': 'dddddd',
+    //   '600': 'e53935',
+    //   '700': 'd32f2f',
+    //   '800': 'c62828',
+    //   '900': 'b71c1c',
+    //   'A100': 'ff8a80',
+    //   'A200': 'ff5252',
+    //   'A400': 'ff1744',
+    //   'A700': 'd50000',
+    // });
+    
     // Toast notification themes
     $mdThemingProvider.theme('toast-default')
     $mdThemingProvider.theme('toast-success');
     $mdThemingProvider.theme('toast-error');
+    
+    $mdThemingProvider.theme('default')
+      .primaryPalette('purple')
+      .accentPalette('amber');
     
     // Loaders
     $mdThemingProvider.theme('progress-default').primaryPalette('green', {
